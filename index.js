94e828f6e650a674fc5547338f351562aed635cc
Robot <register@guanghe.tv>
2016-12-02 14:36:42 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+ar express = require('express');
+
+// Constants
+var PORT = 8088;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
